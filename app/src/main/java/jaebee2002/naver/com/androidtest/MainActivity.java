d6eb2aa3d05bf745340d377e6d226af915c966c9
jaehyun <jaebee2002@naver.com>
2018-09-14 14:44:29 +0900

    채팅 코드 작성

--- app/src/main/java/jaebee2002/naver/com/androidtest/MainActivity.java
+++ app/src/main/java/jaebee2002/naver/com/androidtest/MainActivity.java
@@ -2,13 +2,28 @@ package jaebee2002.naver.com.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    DatabaseReference myRef = database.getReference();
+    ListView listView;
+    EditText editText;
+    Button sendButton;
+    String userName;
+    ArrayAdapter<Object> adapter;
 
 
     @Override
@@ -16,5 +31,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myRef.setValue("Hello, World!");
+        listView = findViewById(R.id.listView);
+        editText = findViewById(R.id.editText);
+        sendButton = findViewById(R.id.button);
+
+        userName = "user" + new Random().nextInt(10000);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        listView.setAdapter(adapter);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChatData chatData = new ChatData(userName, editText.getText().toString());
+                myRef.child("message").push().setValue(chatData);
+                editText.setText("");
+            }
+        });
+        myRef.child("message").addChildEventListener(new ChildEventListener() {  // message는 child의 이벤트를 수신합니다.
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                ChatData chatData = dataSnapshot.getValue(ChatData.class);  // chatData를 가져오고
+                adapter.add(chatData.getUserName() + ": " + chatData.getMessage());  // adapter에 추가합니다.
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
     }
 }
