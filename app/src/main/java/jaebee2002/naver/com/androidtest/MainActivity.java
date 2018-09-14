e669f2360fa4aaa1c1265c84a72ec9063c8a2955
jaehyun <jaebee2002@naver.com>
2018-09-14 14:27:42 +0900

    firebas 데이터베이스에 텍스트 추가

--- app/src/main/java/jaebee2002/naver/com/androidtest/MainActivity.java
+++ app/src/main/java/jaebee2002/naver/com/androidtest/MainActivity.java
@@ -3,11 +3,18 @@ package jaebee2002.naver.com.androidtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myRef.setValue("Hello, World!");
     }
 }
